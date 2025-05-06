d2cf65a360776b17a65e54dd5fb7fb5f1aaa7add
Joaogalescky <joaocvgalescky@gmail.com>
2025-05-06 15:34:11 -0300

    feat: componente Button

--- hello-svelte/src/components/Button.svelte
+++ hello-svelte/src/components/Button.svelte
@@ -1,13 +1,23 @@
 <script lang="ts">
-    export let text: string
+	export let text: string;
+    export let double: boolean = false;
+    export let triple: boolean = false;
 </script>
 
-<button class="button">
-    {text}
+<button class="button" class:triple class:double> <!-- ${triple ? 'triple' : ''}`} -->
+	{text}
 </button>
 
 <style>
-    .button{
+	.button {
+		flex-basis: 25%;
+	}
 
-    }
+	.double {
+        flex-basis: 50%;
+	}
+
+	.triple {
+        flex-basis: 75%;
+	}
 </style>
--- hello-svelte/src/components/Calculator.svelte
+++ hello-svelte/src/components/Calculator.svelte
@@ -1,21 +1,44 @@
 <script lang="ts">
-  import Button from "./Button.svelte";
-
+	import Button from "./Button.svelte";
+	import Line from "./Line.svelte";
 </script>
 
 <div class="calculator">
-    <Button text="7"/>
-    <Button text="8"/>
-    <Button text="9"/>
+	<Line>
+		<Button triple text="AC" />
+        <Button text="/" />
+	</Line>
+	<Line>
+		<Button text="7" />
+		<Button text="8" />
+		<Button text="9" />
+		<Button text="*" />
+	</Line>
+	<Line>
+		<Button text="4" />
+		<Button text="5" />
+		<Button text="6" />
+		<Button text="-" />
+	</Line>
+	<Line>
+		<Button text="1" />
+		<Button text="2" />
+		<Button text="3" />
+		<Button text="+" />
+	</Line>
+	<Line>
+		<Button double text="0" />
+		<Button text="," />
+		<Button text="=" />
+	</Line>
 </div>
 
 <style>
-    .calculator {
-        background-color: var(--background-color);
-        display: flex;
-        flex-direction: column;
-        height: 200px;
-        width: 150px;
-    }
+	.calculator {
+		background-color: var(--background-color);
+		display: flex;
+		flex-direction: column;
+		height: 200px;
+		width: 150px;
+	}
 </style>
-
