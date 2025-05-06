358c1f2af5f922de01cb28a18f13453b937e24c4
Joaogalescky <joaocvgalescky@gmail.com>
2025-05-06 14:46:30 -0300

    feat: componente Button

--- hello-svelte/src/App.svelte
+++ hello-svelte/src/App.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
-	export let name: string;
+  import Button from "./components/Button.svelte";
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Button text="1"/>
+	<Button text="2"/>
+	<Button text="3"/>
 </main>
 
 <style>
---
+++ hello-svelte/src/components/Button.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    export let text: string
+</script>
+
+<button class="button">
+    {text}
+</button>
+
+<style>
+    .button{
+
+    }
+</style>
