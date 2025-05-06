a4350abcd72b7bd07d3866879e744de8e7b0d619
Joaogalescky <joaocvgalescky@gmail.com>
2025-05-06 15:03:44 -0300

    feat: componente Calculator

---
+++ hello-svelte/src/components/Calculator.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import Button from "./Button.svelte";
+
+</script>
+
+<div class="calculator">
+    <Button text="7"/>
+    <Button text="8"/>
+    <Button text="9"/>
+</div>
+
+<style>
+    .calculator {
+        background-color: var(--background-color);
+        display: flex;
+        flex-direction: column;
+        height: 200px;
+        width: 150px;
+    }
+</style>
+
